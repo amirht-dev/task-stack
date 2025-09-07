890883735cf7860412c44b720ec9a1b73ee22a4d
Amir Hossein <amirht.dev@gmail.com>
2025-09-07 22:36:13 +0330

    initiate appwrite

---
+++ src/lib/appwrite/server.ts
@@ -0,0 +1,36 @@
+import 'server-only';
+
+import { cookies } from 'next/headers';
+import { Account, Client } from 'node-appwrite';
+
+export async function createSessionClient() {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
+
+  const session = (await cookies()).get('session');
+  if (!session || !session.value) {
+    throw new Error('No session');
+  }
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+}
+
+export async function createAdminClient() {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID)
+    .setKey(process.env.NEXT_APPWRITE_KEY);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+}
---
+++ src/types/env.d.ts
@@ -0,0 +1,8 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_APPWRITE_PROJECT_ID: string;
+    NEXT_PUBLIC_APPWRITE_PROJECT_NAME: string;
+    NEXT_PUBLIC_APPWRITE_ENDPOINT: string;
+    NEXT_APPWRITE_KEY: string;
+  }
+}
