cb0c84fd17942d986655f0006fb3d648c67fbb27
Amir Hossein <amirht.dev@gmail.com>
2025-09-13 01:15:59 +0330

    refactor auth actions

--- src/actions/auth.action.ts
+++ src/actions/auth.action.ts
@@ -3,15 +3,10 @@
 import { SESSION_COOKIE_KEY } from '@/constants/auth';
 import { createAdminClient, createSessionClient } from '@/lib/appwrite/server';
 import { ServerFunction } from '@/types/next';
-import {
-  DiscriminatedResponse,
-  DiscriminatedResponseWithData,
-} from '@/types/utils';
 import { OAuthSchemaType, SignInSchemaType } from '@/utils/schemas';
-import { setSessionCookie } from '@/utils/server';
+import { handleResponse, setSessionCookie } from '@/utils/server';
 import { cookies } from 'next/headers';
-import { ID, Models, OAuthProvider } from 'node-appwrite';
-import { Promisable } from 'type-fest';
+import { ID, OAuthProvider } from 'node-appwrite';
 
 export const signupAction: ServerFunction<
   [credentials: { email: string; password: string }]
@@ -25,124 +20,53 @@ export const signupAction: ServerFunction<
   });
 };
 
-async function handleError(
-  cb: () => Promisable<void>
-): Promise<DiscriminatedResponse>;
-async function handleError<T>(
-  cb: () => Promisable<T>
-): Promise<DiscriminatedResponseWithData<T>>;
-async function handleError(
-  cb: () => unknown
-): Promise<DiscriminatedResponse | DiscriminatedResponseWithData<unknown>> {
-  try {
-    const data = await cb();
-    return {
-      success: true,
-      data,
-    };
-  } catch (error) {
-    return {
-      success: false,
-      error:
-        error instanceof Error
-          ? error.message
-          : typeof error === 'string'
-          ? error
-          : 'some',
-    };
-  }
-}
-
 export const emailPasswordSigninAction = async ({
   email,
   password,
 }: SignInSchemaType) => {
-  return handleError(async () => {
+  return handleResponse(async () => {
     const { account } = await createAdminClient();
-
     const session = await account.createEmailPasswordSession({
       email,
       password,
     });
-
     await setSessionCookie(session);
   });
 };
 
-export const signoutAction: ServerFunction<
-  [],
-  DiscriminatedResponse
-> = async () => {
-  try {
+export const signoutAction = async () => {
+  return handleResponse(async () => {
     const { account } = await createSessionClient();
     await account.deleteSession({ sessionId: 'current' });
     (await cookies()).delete(SESSION_COOKIE_KEY);
-    return { success: true };
-  } catch (error) {
-    return {
-      success: false,
-      error: error instanceof Error ? error.message : 'failed to signout',
-    };
-  }
-};
-
-export const oauthGetURLAction: ServerFunction<
-  [provider: OAuthProvider, options?: { redirectTo?: string | false }],
-  string
-> = async (provider) => {
-  const { account } = await createAdminClient();
-
-  const successURL = new URL(
-    '/oauth/callback',
-    process.env.NEXT_PUBLIC_ORIGIN_URL
-  );
-
-  const url = await account.createOAuth2Token({
-    provider,
-    success: successURL.toString(),
   });
-
-  return url;
 };
 
-export const oauthSigninAction: ServerFunction<
-  [data: OAuthSchemaType],
-  { success: true } | { success: false; error: string }
-> = async (data) => {
-  try {
+export const oauthGetURLAction = async (provider: OAuthProvider) => {
+  return handleResponse(async () => {
     const { account } = await createAdminClient();
-
-    const session = await account.createSession(data);
-
-    await setSessionCookie(session);
-
-    return { success: true };
-  } catch (error) {
-    return {
-      success: false,
-      error:
-        error instanceof Error
-          ? error.message
-          : typeof error === 'string'
-          ? error
-          : 'failed to signin with oauth',
-    };
-  }
+    const successURL = new URL(
+      '/oauth/callback',
+      process.env.NEXT_PUBLIC_ORIGIN_URL
+    );
+    return await account.createOAuth2Token({
+      provider,
+      success: successURL.toString(),
+    });
+  });
 };
 
-export const getCurrentUserAction: ServerFunction<
-  [],
-  DiscriminatedResponseWithData<Models.User<Models.Preferences>>
-> = async () => {
-  try {
+export const oauthSigninAction = async (data: OAuthSchemaType) => {
+  return handleResponse(async () => {
+    const { account } = await createAdminClient();
+    const session = await account.createSession(data);
+    await setSessionCookie(session);
+  });
+};
+
+export const getCurrentUserAction = async () => {
+  return handleResponse(async () => {
     const { account } = await createSessionClient();
-    const user = await account.get();
-    return { success: true, data: user };
-  } catch (error) {
-    return {
-      success: false,
-      error:
-        error instanceof Error ? error.message : 'failed to get current user',
-    };
-  }
+    return await account.get();
+  });
 };
--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,7 +1,12 @@
 import { SESSION_COOKIE_KEY } from '@/constants/auth';
+import {
+  DiscriminatedResponse,
+  DiscriminatedResponseWithData,
+} from '@/types/utils';
 import { cookies } from 'next/headers';
 import { Models } from 'node-appwrite';
 import 'server-only';
+import { Promisable } from 'type-fest';
 
 export async function setSessionCookie(session: Models.Session) {
   (await cookies()).set(SESSION_COOKIE_KEY, session.secret, {
@@ -10,4 +15,32 @@ export async function setSessionCookie(session: Models.Session) {
     path: '/',
     expires: new Date(session.expire),
   });
+}
+
+export async function handleResponse(
+  cb: () => Promisable<void>
+): Promise<DiscriminatedResponse>;
+export async function handleResponse<T>(
+  cb: () => Promisable<T>
+): Promise<DiscriminatedResponseWithData<T>>;
+export async function handleResponse(
+  cb: () => unknown
+): Promise<DiscriminatedResponse | DiscriminatedResponseWithData<unknown>> {
+  try {
+    const data = await cb();
+    return {
+      success: true,
+      data,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error:
+        error instanceof Error
+          ? error.message
+          : typeof error === 'string'
+          ? error
+          : 'something wrong happened please try again later',
+    };
+  }
 }
