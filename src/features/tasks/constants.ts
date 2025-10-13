44538e650f16c1576c348f998319060c550383cc
Amir Hossein <amirht.dev@gmail.com>
2025-10-13 18:39:36 +0330

    add task status badge colors for light mode

--- src/features/tasks/constants.ts
+++ src/features/tasks/constants.ts
@@ -2,12 +2,13 @@ import { TaskStatus } from './types';
 
 export const taskStatusColorClassName: Record<TaskStatus, string> = {
   BACKLOG:
-    'dark:bg-slate-800/50 dark:border-slate-400 dark:dark:text-slate-400',
-  TODO: 'dark:bg-indigo-800/50 dark:border-indigo-400 dark:dark:text-indigo-400',
+    'dark:bg-slate-800/50 dark:border-slate-400 dark:dark:text-slate-400 bg-slate-300/30 text-slate-700 border-slate-700',
+  TODO: 'dark:bg-indigo-800/50 dark:border-indigo-400 dark:dark:text-indigo-400 bg-indigo-300/30 text-indigo-700 border-indigo-700',
   IN_PROGRESS:
-    'dark:bg-teal-800/50 dark:border-teal-400 dark:dark:text-teal-400',
-  IN_REVIEW: 'dark:bg-rose-800/50 dark:border-rose-400 dark:dark:text-rose-400',
-  DONE: 'dark:bg-green-800/50 dark:border-green-400 dark:text-green-400',
+    'dark:bg-teal-800/50 dark:border-teal-400 dark:dark:text-teal-400 bg-teal-300/30 text-teal-700 border-teal-700',
+  IN_REVIEW:
+    'dark:bg-rose-800/50 dark:border-rose-400 dark:dark:text-rose-400 bg-rose-300/30 text-rose-700 border-rose-700',
+  DONE: 'dark:bg-green-800/50 dark:border-green-400 dark:text-green-400 bg-green-300/30 text-green-700 border-green-700',
 };
 
 export const taskStatusLabelMap: Record<TaskStatus, string> = {
